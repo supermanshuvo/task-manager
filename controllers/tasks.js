150d502596df0f4b9a507f36cf2729ba614d4fa4
Rezaul Karim <shuvothesuperman@gmail.com>
2022-07-17 11:43:16 +0600

    create controller task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,5 +1,16 @@
 const getAllTasks = (req,res)=>{
     res.send('All item');
 };
-
+const createTaks = (req,res)=>{
+    res.send('create Task')
+}
+const createTaks = (req,res)=>{
+    res.send('create Task')
+}
+const createTaks = (req,res)=>{
+    res.send('create Task')
+}
+const createTaks = (req,res)=>{
+    res.send('create Task')
+}
 module.exports = {getAllTasks};
