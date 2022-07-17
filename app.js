f7c236156a167512af5530f53515b65e7ab07ee0
Rezaul Karim <shuvothesuperman@gmail.com>
2022-07-17 10:46:28 +0600

    get router create

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/hello',(req,res)=>{
+   res.send('Task Manager app');
+});
+app.listen(port,function (req,res){
+   console.log(`Running port : ${port}`);
+});
