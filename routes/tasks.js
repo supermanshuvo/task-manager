ac9c2d45a40c381b097ff9fe448e5173f695e2d0
Rezaul Karim <shuvothesuperman@gmail.com>
2022-07-17 11:55:37 +0600

    all router add in router task

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {getAllTasks,createTaks,getTaks,updateTaks,deleteTaks} = require('../controllers/tasks');
-router.route('/').get(getAllTasks);
+router.route('/').get(getAllTasks).post(createTaks);
+router.route('/:id').get(getTaks).patch(updateTaks).delete(deleteTaks);
 
 module.exports = router;
