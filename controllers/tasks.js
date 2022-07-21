33b8d4c31e580a7ab9670ac52c9a22485818bfbb
Rezaul Karim <shuvothesuperman@gmail.com>
2022-07-21 15:54:58 +0600

    get data done

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,7 +1,12 @@
 const Task = require('../models/Task')
 
-const getAllTasks = (req,res)=>{
-    res.send('All item');
+const getAllTasks = async (req,res)=>{
+    try{
+        const task = await Task.find({})
+        res.status(200).json({task});
+    }catch (error){
+        res.status(500).json({msg:error});
+    }
 };
 const createTask = async (req,res)=>{
     try{
