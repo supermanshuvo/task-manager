871b9565ec7d24d4e36e858e5f11f84ef48f25d9
Rezaul Karim <shuvothesuperman@gmail.com>
2022-07-21 13:03:34 +0600

    successfully connect to the database

--- db/connect.js
+++ db/connect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const connection ='mongodb+srv://shuvo:1234@nodeexpressproject.jexqk.mongodb.net/task-manager?retryWrites=true&w=majority;'
 mongoose
     .connect(connection,
-        useNewUrlParser: true,
+        {useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true,}
     )
     .then(()=>console.log('CONNECTED TO THE DB...'))
     .catch((err)=>console.log(err));
