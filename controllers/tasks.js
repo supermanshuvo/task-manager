f80c460075649e0d320f7e2d7f1caa32de9d38e5
Rezaul Karim <shuvothesuperman@gmail.com>
2022-07-21 16:27:20 +0600

    delete is working and done

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,4 +1,5 @@
 const Task = require('../models/Task')
+const {raw} = require("express");
 
 const getAllTasks = async (req,res)=>{
     try{
@@ -16,14 +17,32 @@ const createTask = async (req,res)=>{
         res.status(500).json({msg: error});
     }
 }
-const getTask = (req,res)=>{
-    res.json({id:req.params.id});
+const getTask = async (req,res)=>{
+    try{
+        const {id:taskID} = req.params;
+        const task = await Task.findOne({_id:taskID});
+        if(!task){
+            return res.status(404).json({msg:`No task with id: ${taskID}`});
+        }
+        res.status(200).json({task});
+    }catch (error){
+        res.status(500).json({msg: error});
+    }
 }
 const updateTask = (req,res)=>{
     res.send('update Single Task')
 }
-const deleteTask = (req,res)=>{
-    res.send('delete single Task')
+const deleteTask = async (req,res)=>{
+    try{
+        const {id:taskID} = req.params;
+        const task = await Task.findOneAndDelete({_id:taskID});
+        if(!task){
+            return res.status(404).json({msg:`No task with id: ${taskID}`});
+        }
+        res.status(200).json({task:null,status:'success'});
+    }catch (error){
+        res.status(500).json({msg: error});
+    }
 }
 module.exports = {
     getAllTasks,createTask,getTask,updateTask,deleteTask
