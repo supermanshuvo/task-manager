c2077ac1c8d0365cc33a7c6f5bdfac86b87c1eba
Rezaul Karim <shuvothesuperman@gmail.com>
2022-07-21 16:49:57 +0600

    update is done

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -45,7 +45,13 @@ const deleteTask = async (req,res)=>{
 const updateTask = async (req,res)=>{
     try{
         const {id:taskID} = req.params;
-        res.status(200).json({id:taskID,data:req.body});
+        const task = await Task.findOneAndUpdate({_id:taskID},req.body,{
+            new:true,runValidators:true
+        });
+        if(!task){
+            return res.status(404).json({msg:`No task with id: ${taskID}`});
+        }
+        res.status(200).json({task});
     }catch (error){
         res.status(500).json({msg: error});
     }
