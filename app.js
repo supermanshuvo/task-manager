7f908eab2f98c7f619d8659afe7401be63c1af05
Rezaul Karim <shuvothesuperman@gmail.com>
2022-07-21 13:21:50 +0600

    fix the connection problem and create model folder

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
-
+require('dotenv').config();
 
 // Middleware
 app.use(express.json());
@@ -21,7 +21,7 @@ const port = 3000;
 
 const start = async ()=>{
    try{
-      await connectDB();
+      await connectDB(process.env.MONGO_URL);
       app.listen(port,function (req,res){
          console.log(`Running port : ${port}`);
       });
--- db/connect.js
+++ db/connect.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
-const connection ='mongodb+srv://shuvo:1234@nodeexpressproject.jexqk.mongodb.net/task-manager?retryWrites=true&w=majority;';
 
-const connectDB = () =>{
-    return mongoose.connect(connection,
+const connectDB = (url) =>{
+    return mongoose.connect(url,
             {useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true,}
         )
 };
