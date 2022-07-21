72e3409d29b3d014ca10ec2541411900ecceaf2b
Rezaul Karim <shuvothesuperman@gmail.com>
2022-07-21 13:59:14 +0600

    error in sending data

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -4,8 +4,12 @@ const getAllTasks = (req,res)=>{
     res.send('All item');
 };
 const createTaks = async (req,res)=>{
-    const task = await Task.create(req.body);
-    res.status(201).json({task})
+    try{
+        const task = await Task.create(req.body);
+        res.status(201).json({task});
+    }catch (error){
+        res.status(500).json({msg: error});
+    }
 }
 const getTaks = (req,res)=>{
     res.json({id:req.params.id});
