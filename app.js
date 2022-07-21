a500e196cc7188ae4ff80faabaf6199dd4dcc4d4
Rezaul Karim <shuvothesuperman@gmail.com>
2022-07-21 13:12:05 +0600

    successfully connect with data base and working file

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-require('./db/connect');
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
-const port = 3000;
+const connectDB = require('./db/connect');
+
 
 // Middleware
 app.use(express.json());
@@ -17,7 +17,17 @@ app.get('/hello',(req,res)=>{
 app.use('/api/v1/tasks',tasks);
 
 
+const port = 3000;
 
-app.listen(port,function (req,res){
-   console.log(`Running port : ${port}`);
-});
+const start = async ()=>{
+   try{
+      await connectDB();
+      app.listen(port,function (req,res){
+         console.log(`Running port : ${port}`);
+      });
+   }catch (error){
+      console.log(error);
+   }
+}
+
+start();
--- db/connect.js
+++ db/connect.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
-const connection ='mongodb+srv://shuvo:1234@nodeexpressproject.jexqk.mongodb.net/task-manager?retryWrites=true&w=majority;'
-mongoose
-    .connect(connection,
-        {useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true,}
-    )
-    .then(()=>console.log('CONNECTED TO THE DB...'))
-    .catch((err)=>console.log(err));
+const connection ='mongodb+srv://shuvo:1234@nodeexpressproject.jexqk.mongodb.net/task-manager?retryWrites=true&w=majority;';
+
+const connectDB = () =>{
+    return mongoose.connect(connection,
+            {useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true,}
+        )
+};
+module.exports = connectDB;
