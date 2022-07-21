5f278166e5269456042b4bbef38f0e08d0a89184
Rezaul Karim <shuvothesuperman@gmail.com>
2022-07-21 13:00:59 +0600

    got error

--- db/connect.js
+++ db/connect.js
@@ -1,3 +1,8 @@
 const mongoose = require('mongoose');
 const connection ='mongodb+srv://shuvo:1234@nodeexpressproject.jexqk.mongodb.net/task-manager?retryWrites=true&w=majority;'
-mongoose.connect(connection).then(()=>console.log('CONNECTED TO THE DB...')).catch((err)=>console.log(err));
+mongoose
+    .connect(connection,
+        useNewUrlParser: true,
+    )
+    .then(()=>console.log('CONNECTED TO THE DB...'))
+    .catch((err)=>console.log(err));
